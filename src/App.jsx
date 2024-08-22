c32d53f5858d61a6aa48e6f000b003242a2d5f26
Liliana Benitez <lilyybenitezz@gmail.com>
2024-08-21 23:02:06 -0400

    logo and copyright

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 /* eslint-disable react/no-unescaped-entities */
 // import { useState } from 'react'
 import arcadeGame from './assets/arcade-game.png'
+import logo from '../public/pixelmilo.png'
 
 function App() {
 
   return (
-    <body className="bg-black text-white h-screen flex flex-col justify-between p-4 font-pixelify">
-    <h1 className="text-6xl font-bold text-center text-sky-400">
-      MILO'S MINI ARCADE
-    </h1>
+    <body className="bg-black text-white h-screen flex flex-col items-center justify-between p-4 font-pixelify">
+    
+    <div className='flex flex-col items-center'>
+      <img src={logo} height={80} width={80}/>
+      <h1 className="text-6xl font-bold text-center text-sky-400">
+        MILO'S MINI ARCADE
+      </h1>
+    </div>
 
     <section className="grid grid-cols-3 gap-7 place-items-center">
       <div className="border rounded-md border-yellow-300 p-4 h-52 w-52 flex flex-col justify-center items-center text-sky-400 text-xl">
@@ -37,8 +42,8 @@ function App() {
       </div>
     </section>
 
-    <footer className="text-center text-slate-800">
-      <h6> © COPYRIGHT TINITO CODE STUDIOS 2024</h6>
+    <footer className="text-center text-yellow-200">
+      <h6> made with 💙 by Tinito Code Studios</h6>
     </footer>
     </body>
   )
